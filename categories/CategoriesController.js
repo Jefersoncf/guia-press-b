4641eb8cf227ea143c401b3d6fca05109ff5610d
Jeferson Ferreira <fjeferson60@gmail.com>
2022-06-15 07:03:38 -0300

    Cadastrando categorias no banco

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
 
-router.get('/categories', (req, res) => {
-  res.send('Minha rota de categorias');
+router.get('/admin/categories/new', (req, res) => {
+  res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+  let title = req.body.title;
+
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title)
+    }).then(() => {
+      res.redirect('/');
+    })
+  }else {
+    res.redirect('/admin/categories/new');
+  }
 });
 
 module.exports = router;
