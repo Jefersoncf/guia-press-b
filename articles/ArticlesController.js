7cc3d01874e9e1d549007043d185ecf7f8d64f55
Jeferson Ferreira <fjeferson60@gmail.com>
2022-07-08 22:59:28 -0300

    Adicionando rota de deleção de artigos e criando home page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,12 +5,16 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-  res.render("admin/articles/index");
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
   Category.findAll().then((categories) => {
-    res.render("/admin/articles/new", { categories: categories });
+    res.render("admin/articles/new", { categories: categories });
   });
 });
 
@@ -27,6 +31,23 @@ router.post("/articles/save", (req, res) => {
   }).then(() => {
     res.redirect("/admin/articles");
   });
+});
+
+router.post("/articles/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({ where: { id: id } }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      //não for um number
+      res.redirect("/admin/articles");
+    }
+  } else {
+    //null
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = router;
